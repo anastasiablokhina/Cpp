2573bfdef4e6d99549a3fbfc4e852f7680f431e5
anastasiablokhina <71010324+anastasiablokhina@users.noreply.github.com>
2022-11-19 19:15:17 +0300

    Практическая работа №4

---
+++ lab_4/ex1.cpp
@@ -0,0 +1,49 @@
+﻿// Вычисление корней квадратного уравнения
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int Myroot(double a, double b, double c, double& x_1, double& x_2)
+{
+    double d = pow(b, 2) - 4 * a * c; // дискриминант
+
+    if (d > 0) // если дискриминант положительный, то корня два
+    {
+        x_1 = (-b + sqrt(d)) / (2 * a);
+        x_2 = (-b - sqrt(d)) / (2 * a);
+        return 1;
+    }
+    else
+        if (d == 0) // если равен нулю, то корень один
+        {
+            x_1 = (-b + sqrt(d)) / (2 * a);
+            return 0;
+        }
+        else // если меньше нуля, корней нет
+            return -1;
+}
+
+int main() {
+
+    int res;
+    float a, b, c;
+    double x_1 = 0, x_2 = 0;
+
+
+    cout << "Enter the coefficients ";
+    cin >> a >> b >> c;
+    res = Myroot(a, b, c, x_1, x_2);
+
+    if (res == 1)
+        cout << "There are two roots of the equation: x1 = " << x_1 << " and x2 = " << x_2;
+    else
+        if (res == 0)
+            cout << "There is one root of the equation: x = " << x_1;
+        else
+            if (res == -1)
+                cout << "There are no roots of the equation";
+
+    return 0;
+}
+
---
+++ lab_4/ex2.cpp
@@ -0,0 +1,25 @@
+﻿// Реализация ввода данных
+
+#include <iostream>
+using namespace std;
+
+bool Input(int x, int y) // проверка, что числа положительные
+{
+    return (x > 0 and y > 0);
+}
+
+int main()
+{
+    int a, b;
+    cout << "Enter two positive numbers "; 
+    cin >> a >> b; // ввод двух положительных чисел
+    if (Input(a, b) == false) // если ввод некорректен, то выводится сообщение об ошибке, программа завершается
+    {
+        cerr << "error";
+        return 1;
+    }
+    int s = a + b; // вычисление суммы
+    cout << a << " + " << b << " = " << s; // вывод
+    return 0;
+}
+
