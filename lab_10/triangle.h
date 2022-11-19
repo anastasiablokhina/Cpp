84a1c4c1ebb596cdd3ff54b8ae993532f13f081a
anastasiablokhina <71010324+anastasiablokhina@users.noreply.github.com>
2022-11-19 21:36:33 +0300

    Update triangle.h

--- lab_10/triangle.h
+++ lab_10/triangle.h
@@ -7,8 +7,8 @@ private:
 	Dot* d1, d2, d3;
 	double a, b, c;
 public:
-	Triangle(double x1, double y1, double x2, double y2, double x3, double y3); // композиция
-	Triangle(Dot* d1, Dot* d2, Dot* d3); // агрегация
+	Triangle(double x1, double y1, double x2, double y2, double x3, double y3); // РєРѕРјРїРѕР·РёС†РёСЏ
+	Triangle(Dot* d1, Dot* d2, Dot* d3); // Р°РіСЂРµРіР°С†РёСЏ
 	void getAllSides();
 	double square();
 	double perimeter();
