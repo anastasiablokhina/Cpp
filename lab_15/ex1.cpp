417f936ff732ec935b9da88429c08cd94c432c04
anastasiablokhina <71010324+anastasiablokhina@users.noreply.github.com>
2022-11-21 00:52:42 +0300

    Практическая работа №15

---
+++ lab_15/ex1.cpp
@@ -0,0 +1,46 @@
+﻿// Организация хранения данных в контейнере
+
+#include <iostream>
+#include <string>
+#include <map>
+using namespace std;
+
+struct StudentGrade
+{
+    string name;
+    char grade;
+    StudentGrade(string name, char grade)
+    {
+        this->name = name;
+        this->grade = grade;
+    }
+};
+
+int main()
+{ 
+    map<string, char> gradebook; // контейнер журнал
+    int n; // количество студентов в группе
+
+    cout << "Enter the number of students ";
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+    {
+        string name;
+        char grade;
+        char choice;
+
+        cout << "Enter student name and grade ";
+        cin >> name >> grade;
+  
+        StudentGrade studentGrade = StudentGrade(name, grade);
+        gradebook[studentGrade.name] = studentGrade.grade;    
+    }
+
+    for (const auto& student : gradebook)
+    {
+        cout << "Student: " << student.first << ", Grade: " << student.second << endl;
+    }
+
+    return 0;
+}
